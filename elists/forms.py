870d93f0b197bb3a9e0610156dfb68e6b0b1c7bc
AlekseiKuzhovnik <113526341+AlekseiKuzhovnik@users.noreply.github.com>
2023-01-14 21:30:07 +0100

    update posts

--- elists/forms.py
+++ elists/forms.py
@@ -1,17 +1,17 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from .models import Event
-from datetime import datetime
+from .models import Event, EventPlan
+from datetime import datetime, timedelta
 
 
 class CreateEvent(forms.ModelForm):
-    start_date = forms.DateTimeField(initial=datetime.now(), label='Дата начала')
-    end_date = forms.DateTimeField(initial=datetime.now(), label='Дата завершения')
+    start_date = forms.DateTimeField(initial=datetime.today().strftime("%d.%m.%Y %H:%M"), label='Дата начала')
+    end_date = forms.DateTimeField(initial=(datetime.today() + timedelta(hours=1)).strftime("%d.%m.%Y %H:%M"), label='Дата завершения')
 
     class Meta:
         model = Event
         fields = ('event_name', 'short_description', 'full_description', 'start_date', 'end_date', 'image')
-        labels = {'event_name': 'Название', 'short_description': 'Короткое описание',
+        labels = {'event_name': 'Название*', 'short_description': 'Короткое описание*',
                   'full_description': 'Полное описание', 'image': 'Добавить изображение'}
 
         # widgets = {
@@ -42,13 +42,13 @@ class CreateEvent(forms.ModelForm):
 
 
 class UpdateEvent(forms.ModelForm):
-    start_date = forms.DateTimeField(initial=datetime.now(), label='Дата начала')
-    end_date = forms.DateTimeField(initial=datetime.now(), label='Дата завершения')
+    start_date = forms.DateTimeField(initial=datetime.today().strftime("%d.%m.%Y %H:%M"), label='Дата начала')
+    end_date = forms.DateTimeField(initial=(datetime.today() + timedelta(hours=1)).strftime("%d.%m.%Y %H:%M"), label='Дата завершения')
 
     class Meta:
         model = Event
         fields = ('event_name', 'short_description', 'full_description', 'start_date', 'end_date')
-        labels = {'event_name': 'Название', 'short_description': 'Короткое описание',
+        labels = {'event_name': 'Название*', 'short_description': 'Короткое описание*',
                   'full_description': 'Полное описание'}
 
     def __init__(self, *args, **kwargs):
@@ -61,17 +61,18 @@ class UpdateEvent(forms.ModelForm):
         self.fields['start_date'].widget.attrs.update({'class': 'form-control form-control-sm'}, placeholder="yyyy-mm-dd hh:mi")
         self.fields['end_date'].widget.attrs.update({'class': 'form-control form-control-sm'}, placeholder="yyyy-mm-dd hh:mi")
 
-    def clean_end_date(self):
-        start_date_data = self.cleaned_data['start_date']
-        end_date_data = self.cleaned_data['end_date']
-
-        if start_date_data >= end_date_data:
-            raise ValidationError("Дата завершения не может быть раньше даты начала")
-
-        return end_date_data
+    # def clean_end_date(self):
+    #     start_date_data = self.cleaned_data['start_date']
+    #     end_date_data = self.cleaned_data['end_date']
+    #
+    #     if start_date_data >= end_date_data:
+    #         raise ValidationError("Дата завершения не может быть раньше даты начала")
+    #
+    #     return end_date_data
 
 
 class UpdateImageEvent(forms.ModelForm):
+    image = forms.ImageField()
 
     class Meta:
         model = Event
@@ -81,5 +82,34 @@ class UpdateImageEvent(forms.ModelForm):
     def __init__(self, *args, **kwargs):
 
         super(UpdateImageEvent, self).__init__(*args, **kwargs)
-
         self.fields['image'].widget.attrs.update({'class': 'form-control-file'})
+
+
+class CreateEventPlan(forms.ModelForm):
+    start_date = forms.DateTimeField(initial=datetime.today().strftime("%d.%m.%Y %H:%M"), label='Дата начала')
+    end_date = forms.DateTimeField(initial=(datetime.today() + timedelta(hours=1)).strftime("%d.%m.%Y %H:%M"), label='Дата завершения')
+
+    class Meta:
+        model = EventPlan
+        fields = ('plan_name', 'plan_short_description', 'plan_full_description', 'start_date', 'end_date')
+        labels = {'plan_name': 'Название*', 'plan_short_description': 'Короткое описание',
+                  'plan_full_description': 'Полное описание'}
+
+    def __init__(self, *args, **kwargs):
+
+        super(CreateEventPlan, self).__init__(*args, **kwargs)
+
+        self.fields['plan_name'].widget.attrs.update({'class': 'form-control form-control-lg'})
+        self.fields['plan_short_description'].widget.attrs.update({'class': 'form-control'})
+        self.fields['plan_full_description'].widget.attrs.update({'class': 'form-control'})
+        self.fields['start_date'].widget.attrs.update({'class': 'form-control form-control-sm'}, placeholder="yyyy-mm-dd hh:mi")
+        self.fields['end_date'].widget.attrs.update({'class': 'form-control form-control-sm'}, placeholder="yyyy-mm-dd hh:mi")
+
+    def clean_end_date(self):
+        start_date_data = self.cleaned_data['start_date']
+        end_date_data = self.cleaned_data['end_date']
+
+        if start_date_data >= end_date_data:
+            raise ValidationError("Дата завершения не может быть раньше даты начала")
+
+        return end_date_data
